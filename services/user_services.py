ede66dc989d26f2125c9d6ec2a0a78deaa247b14
Abhash <abhashgoyal200@gmail.com>
2024-11-28 14:33:26 +0530

    Removed Temp files

--- services/user_services.py
+++
@@ -1,7 +0,0 @@
-"""
-We have a library management system.
-So the user will have two functionalities:
-1. To buy books
-2. To view books
-"""
-
--- utils/jwt_config.py
+++
@@ -1,6 +0,0 @@
-import os
-from datetime import timedelta
-
-SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 300  # Token expiration time
